bed9696533a781aa6f41834dc506b5ec7ce50669
choichoijin <cuj10594@naver.com>
2024-03-22 16:59:02 +0900

    feat: delete item and check password

--- src/main/java/com/md/actionspringboot/actionItems/controller/ActionItemsController.java
+++ src/main/java/com/md/actionspringboot/actionItems/controller/ActionItemsController.java
@@ -1,18 +1,53 @@
 package com.md.actionspringboot.actionItems.controller;
 
+import com.md.actionspringboot.common.dto.PasswordDTO;
+import com.md.actionspringboot.actionItems.service.ActionItemsService;
+import com.md.actionspringboot.common.dto.ResponseDTO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequiredArgsConstructor
 @RequestMapping("/api/action-items")
 public class ActionItemsController {
-    @GetMapping("")
-    public ResponseEntity<?> getTest() {
-        return ResponseEntity.status(HttpStatus.OK).body(null);
+
+    private final ActionItemsService actionItemsService;
+
+    @DeleteMapping("/{actionId}")
+    public ResponseEntity<ResponseDTO> doDeleteItems(@RequestBody PasswordDTO passwordDto,
+                                                             @PathVariable Long actionId) {
+        ResponseDTO responseDTO = new ResponseDTO();
+        try {
+            responseDTO = actionItemsService.deleteActionItems(actionId, passwordDto.getPassword());
+            if (responseDTO.getStatus().equals("success")) {
+                return ResponseEntity.ok().body(responseDTO);
+            } else {
+                return ResponseEntity.badRequest().body(responseDTO);
+            }
+        } catch (Exception e) {
+            responseDTO.setMessage(e.getMessage());
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+
     }
+
+    @PostMapping("/{actionId}")
+    public ResponseEntity<ResponseDTO> doCheckPassword(@RequestBody PasswordDTO passwordDto,
+                                                       @PathVariable Long actionId) {
+        ResponseDTO responseDTO = new ResponseDTO();
+        try {
+            responseDTO = actionItemsService.checkPassword(actionId, passwordDto.getPassword());
+            if (responseDTO.getStatus().equals("success")) {
+                return ResponseEntity.ok().body(responseDTO);
+            } else {
+                return ResponseEntity.badRequest().body(responseDTO);
+            }
+        } catch (Exception e) {
+            responseDTO.setMessage(e.getMessage());
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+    }
+
 }
--- src/main/java/com/md/actionspringboot/actionItems/service/ActionItemsService.java
+++ src/main/java/com/md/actionspringboot/actionItems/service/ActionItemsService.java
@@ -1,4 +1,68 @@
 package com.md.actionspringboot.actionItems.service;
 
+import com.md.actionspringboot.actionItems.entity.ActionItems;
+import com.md.actionspringboot.actionItems.repository.ActionItemsRepository;
+import com.md.actionspringboot.common.dto.ResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
 public class ActionItemsService {
+
+    private final ActionItemsRepository actionItemsRepository;
+
+    @Transactional
+    public ResponseDTO deleteActionItems(Long actionId, String password) {
+
+            Optional<ActionItems> actionItemsOptional = actionItemsRepository.findById(actionId);
+            if (actionItemsOptional.isEmpty()) {
+                return ResponseDTO.builder()
+                        .status("fail")
+                        .message("일치하는 액션 아이템이 없습니다.")
+                        .build();
+            }
+
+            ActionItems actionItems = actionItemsOptional.get();
+            if (!actionItems.getPassword().equals(password)) {
+                return ResponseDTO.builder()
+                        .status("fail")
+                        .message("패스워드가 일치하지 않습니다.")
+                        .build();
+            }
+
+            actionItemsRepository.delete(actionItems);
+            return ResponseDTO.builder()
+                    .status("success")
+                    .message("삭제 성공")
+                    .build();
+
+    }
+
+    @Transactional
+    public ResponseDTO checkPassword(Long actionId, String password) {
+
+        Optional<ActionItems> actionItemsOptional = actionItemsRepository.findById(actionId);
+
+        if (actionItemsOptional.isEmpty()) {
+            return ResponseDTO.builder()
+                    .status("fail")
+                    .message("일치하는 액션 아이템이 없습니다.")
+                    .build();
+        }
+
+        ActionItems actionItems = actionItemsOptional.get();
+        boolean isPasswordMatch = actionItems.getPassword().equals(password);
+        return ResponseDTO.builder()
+                .status("success")
+                .message("비밀번호 확인 성공")
+                .data(isPasswordMatch)
+                .build();
+
+    }
+
 }
