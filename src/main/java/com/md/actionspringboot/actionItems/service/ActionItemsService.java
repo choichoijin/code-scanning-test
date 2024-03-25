06cfc7743f9341694a1bbb9a7bf8192468e1f8bd
choichoijin <cuj10594@naver.com>
2024-03-25 16:34:37 +0900

    chore: add transactional annotation

--- src/main/java/com/md/actionspringboot/actionItems/service/ActionItemsService.java
+++ src/main/java/com/md/actionspringboot/actionItems/service/ActionItemsService.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
+@Transactional
 public class ActionItemsService {
 
     private final ActionItemsRepository actionItemsRepository;
