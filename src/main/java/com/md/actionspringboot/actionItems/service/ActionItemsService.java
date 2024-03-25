83dc3cf20068534e89220af1331a754e60443069
choichoijin <cuj10594@naver.com>
2024-03-25 15:42:32 +0900

    chore: delete unnecessary code

--- src/main/java/com/md/actionspringboot/actionItems/service/ActionItemsService.java
+++ src/main/java/com/md/actionspringboot/actionItems/service/ActionItemsService.java
@@ -11,7 +11,6 @@ import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
-@Transactional(readOnly = true)
 public class ActionItemsService {
 
     private final ActionItemsRepository actionItemsRepository;
